12d2dcb1c89b9b9c3c51eb4946b9c2da38a1aee2
= <leucha@gmail.com>
2022-07-07 18:32:25 -0400

    added unity project to it

---
+++ The Thinker/Assets/Knowledge.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leguar.TotalJSON;
+
+public class Knowledge : MonoBehaviour
+{
+
+    public Taxonomy taxonomy;
+    public Relations relations;
+    public Distributions distributions;
+    public Discrete_Probabilites discrete_probabilities;
+
+    public static void ParseResponse(string text){
+        print(text);
+        Knowledge knowledge = JSON.ParseString(text).Deserialize<Knowledge>();
+        
+    }
+
+    
+    
+    public class Taxonomy
+    {
+        public Dictionary<string,string> children;
+        public Dictionary<string,string> parents;
+    }
+
+    public class Relations
+    {
+        public Dictionary<string, string> entity;
+        public Dictionary<string, string> relationship;
+        public Dictionary<string, int> distribution;
+    }
+
+    public class Distributions
+    {
+        
+        public Dictionary<string, int> id;
+        public Dictionary<string, string> type;
+        public Dictionary<string, float> mean;
+        public Dictionary<string, float> variance;
+        public Dictionary<string, string> discrete_id;
+    }
+
+    
+
+    public class Discrete_Probabilites
+    {
+        public Dictionary<string, string> discrete_id;
+        public Dictionary<string, string>  value;
+        public Dictionary<string, string> value_type;
+        public Dictionary<string, float> probabilitiies; 
+
+
+    }
+
+
+}
+
+
